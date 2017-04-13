df0d36913b9dbc6682186f395c2c58028a0a8b0b
henrique <henrique.contato@outlook.com>
2017-04-13 04:38:35 -0300

    Configuring new webpack.optimize.UglifyJsPlugin()

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
+var inProduction = (process.env.NODE_ENV === "production");
 
 module.exports = {
   entry: './src/main.js',
@@ -19,5 +20,12 @@ module.exports = {
         loader: "babel-loader"
       }
     ]
-  }
+  },
+  plugins: []
 };
+
+if (inProduction) {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
